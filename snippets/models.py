1a3f01515c29e5e1f5135e7f383414917e64444a
Panchi <pandol.francisco@gmail.com>
2016-11-09 15:31:28 -0300

    agregado el campo lenguaje

--- snippets/models.py
+++ snippets/models.py
@@ -7,14 +7,18 @@ from django.db import models
 #LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
 #STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
 
+LANGUAGE_CHOICES = (
+	('PY', 'Python'),
+	('JA', 'Java')
+)
 
 class Snippet(models.Model):
     fecha_creacion = models.DateTimeField(auto_now_add=True)
     titulo = models.CharField(max_length=100, blank=True, default='')
     codigo = models.TextField()
     #linenos = models.BooleanField(default=False)
-    #lenguaje = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
+    lenguaje = models.CharField(choices=LANGUAGE_CHOICES, default=LANGUAGE_CHOICES[0][0], max_length=2)
     #estilo = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
 
     class Meta:
-        ordering = ('created',)
+        ordering = ('fecha_creacion',)
