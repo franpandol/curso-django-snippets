5b995ccb80da000800cfc8bfe4c55b15dd5c79e1
panchipandol <pandol.francisco@gmail.com>
2016-11-23 09:43:33 -0300

    snippets about_us, listado, mejoras en template

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,14 +1,22 @@
 from django.conf.urls import include, url
 from .views import WebIndex, SnippetsList, SnippetsDetail
-
-
+from .views import AboutUs
 urlpatterns = [
+    
     url(r'^$',
         WebIndex.as_view(),
         name='index'),
+
     url(r'^listado/$',
         SnippetsList.as_view(),
         name='list'),
+
+    
+    url(r'^about_us/$',
+        AboutUs.as_view(),
+        name='about-us'),
+
+
     url(r'^(?P<pk>\d+)/$',
         SnippetsDetail.as_view(),
         name='detail'),
--- snippets/views.py
+++ snippets/views.py
@@ -16,12 +16,15 @@ class WebIndex(TemplateView):
     def get_context_data(self, *args, **kwargs):
         # Indicar contenido a mostrar en la página inicial.
         context = super(WebIndex, self).get_context_data(*args, **kwargs)
+        
         snippets = Snippet.objects.all()
-        #fecha = timezone.now()
-
         context['snippets'] = snippets
+        
         #context['fecha'] = fecha
         return context
+
+class AboutUs(TemplateView):
+    template_name = 'snippets/about_us.html'
 
 
 class SnippetsList(ListView):
@@ -30,13 +33,15 @@ class SnippetsList(ListView):
     template_name = 'snippets/list.html'
     #paginate_by = 20
 
+
     def get_context_data(self, *args, **kwargs):
         context = super(SnippetsList, self).get_context_data(*args, **kwargs)
-        #print(context['objects_list'])
+        snippets_count = len(Snippet.objects.all())
+        context['snippets_count'] = snippets_count
+        #context['snippets_list_start_p'] = self.model.objects.filter(titulo__startwith='P')
         return context
 
     def get_queryset(self):
-
         result = self.model.objects.filter(publicado=True)
         return result
 
